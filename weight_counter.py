980a676eb196a9c8070c77687a3d3436ffc13835
CielAl <cielmercy@gmail.com>
2019-03-04 19:37:07 -0500

    by default updating weight by # of patches

--- weight_counter.py
+++ weight_counter.py
@@ -17,8 +17,9 @@
 
 '''
 def weight_counter_filename(obj,totals,file,img,label,extra_infomration):
-	classid=[idx for idx in range(len(obj.database.classes)) if str(obj.database.classes[idx]) in file][0]
-	totals[classid]+=1
+	class_list = [idx for idx in range(len(obj.database.classes)) if str(obj.database.classes[idx]) in file]
+	classid= class_list[0]
+	totals[classid]+= len(class_list)
 	return totals
 
 def weight_counter_maskpixel(obj,totals,file,img,label,extra_infomration):
