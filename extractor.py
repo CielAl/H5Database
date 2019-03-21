9286bdecb7d44a2d235a643a192623aec2a5dc64
CielAl <cielmercy@gmail.com>
2019-03-21 14:20:57 -0400

    the contrast-based tissue screening now accepts params from obj.meta

--- extractor.py
+++ extractor.py
@@ -39,9 +39,9 @@ def extractor_super_resolution(obj,file):
 
 
 
-def getBackground(img_gray,params = {}):
+def getBackground(img_gray,params):
 	img_laplace = np.abs(skimage.filters.laplace(img_gray))
-	mask = (skimage.filters.gaussian(img_laplace, sigma=params.get("variance", 5)) <= params.get("smooth_thresh", 0.03))
+	mask = skimage.filters.gaussian(img_laplace, sigma=params.get("variance") <= params.get("smooth_thresh"))
 	background = (mask!=0) * img_gray
 	background[mask==0] = 1# background[mask_background].mean()
 	return background,mask
@@ -57,8 +57,11 @@ def patch_qualification(patch_sanitized,tissue_threshold_ratio = 0.95):
 		if qualification_no_background_helper(patch,tissue_threshold_ratio):
 			idx_qualified.append(idx)
 	return patch_sanitized[idx_qualified]
-def background_sanitize(image,params={}):
+def background_sanitize(obj,image):
 	img_gray = rgb2gray(image)
+	params =  {}
+	params['variance'] = getattr(obj.meta,'bg_var', 32)
+	params['smooth_thresh'] = getattr(obj.meta,'bg_smooth_thresh', 0.03)
 	background,mask = getBackground(img_gray,params) #- pixel 1/True is the background part
 	image[mask==1] = 0
 	return image
@@ -100,7 +103,7 @@ def extractor_patch_classification(obj,file):
 		image_whole = mask_screening(obj,file,image_whole)
 
 	#make background pixel strictly 0
-	image_whole_sanitized = background_sanitize(image_whole)
+	image_whole_sanitized = background_sanitize(obj,image_whole)
 	data_image_sanitized  = generate_patch(obj,image_whole_sanitized,type = 'img')
 	
 	data_image_qualified= patch_qualification(data_image_sanitized,obj.meta.tissue_area_thresh) 
--- weight_counter.py
+++ weight_counter.py
@@ -21,7 +21,7 @@ def weight_counter_filename(obj,totals,file,img,label,extra_infomration):
 	basename = os.path.basename(file)
 	class_list = [idx for idx in range(len(obj.database.classes)) if re.search(str(obj.database.classes[idx]),basename,re.IGNORECASE)]
 	classid= class_list[0]
-	totals[classid]+= len(class_list)
+	totals[classid]+= len(label)
 	return totals
 
 def weight_counter_maskpixel(obj,totals,file,img,label,extra_infomration):
