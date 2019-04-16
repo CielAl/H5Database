b50cd7331a3f02b7526b225358d0975072c41206
CielAl <cielmercy@gmail.com>
2019-04-16 16:32:28 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+__VERSION__ = '0.2.2'
+
+setup(name='adabound',
+	version=__VERSION__,
+	description='HDF5 Database for Images',
+	long_description=open("README.md").read(),
+	long_description_content_type="text/markdown",
+	keywords=['pytable','hdf5','patch'],
+	classifiers=[
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3.6',
+	],
+	url='https://github.com/CielAl/H5Database',
+	author='***',
+	author_email='**',
+	license='MIT',
+	packages=['h5database'],
+	install_requires=[
+	'tables>=3.4.4',
+	'sklearn>=0.20.2',
+	'numpy>=1.14.5'
+	],
+	zip_safe=False,
+	python_requires='>=3.6.0')
